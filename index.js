d7cac782eaa6dae04ca018e9607ed550f66615ee
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-07-31 22:12:49 +0530

    r

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
+    httpOnly: false,
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: 'strict'
   }
