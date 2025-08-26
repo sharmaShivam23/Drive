de8875711fd9c465f966c304885911eacf6590be
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-08-26 11:41:23 +0530

    r

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -21,7 +21,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
-  max: 20,
+  max: 30,
   message: {
     success: false,
     message: "Too many registration attempts. Please try again after 15 minutes."
