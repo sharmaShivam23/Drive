88d34e01ce59c45b2fe5cf0ab7f482d8b5330639
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-11-13 15:37:44 +0530

    index js

--- index.js
+++ index.js
@@ -133,6 +133,6 @@ process.on('SIGINT', () => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode change`);
+  console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
 });
 
